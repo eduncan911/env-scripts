2f784976fdc5dd59ed191ebcf196e5c261f1fa5f
Eric Duncan <eduncan@singleplatform.com>
2014-09-14 21:11:08 -0400

    initial setup that I'm happy with

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# requirements are to have Dropbox installed at:
+#   ~/Dropbox/
+# as well as have this git repo to reside at:
+#   ~/scripts/
+#
+# then you can run this shell script that will setup
+# the following applications:
+#
+#   * ~/config/SublimeText3/ shared Packages/User/ config files.
+#   * ~/git-prompt for bash cli colorizing
+#   * ~/.bashrc xterm scripts
+#   * ~/.i3/ configurations
+#
+
